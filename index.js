ce408c3109ff94182709324cd596f43cbc4ab67d
Kyle Fossman <kjfossman@gmail.com>
2021-01-17 18:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  return Object.assign(delete object.key)
+  delete object.key
+  return object
 }
